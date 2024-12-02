48a5ccdf5533a9cfbd4faf033bb00f963bcb35ed
Liu Rui <lovearuis@gmail.com>
2024-12-02 17:31:38 +0800

    version-update

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,12 @@ allprojects {
 
     repositories {
         mavenLocal()
-//        maven { url = uri("https://mirrors.cloud.tencent.com/repository/maven") }
-//        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven {
+            url = uri("http://192.168.3.19:8081/repository/maven-public/")
+            isAllowInsecureProtocol = true
+        }
+        maven { url = uri("https://mirrors.cloud.tencent.com/repository/maven") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
         mavenCentral()
     }
 
