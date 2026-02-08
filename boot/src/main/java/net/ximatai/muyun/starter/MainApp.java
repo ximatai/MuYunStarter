c9ed66d4bc674a7e3f874fefa5b0a06aa7bfed5b
Liu Rui <lovearuis@gmail.com>
2026-02-08 22:32:00 +0800

    fix: 入口处要拉取一下 IDatabaseOperations ，不然有可能启动报错

--- boot/src/main/java/net/ximatai/muyun/starter/MainApp.java
+++ boot/src/main/java/net/ximatai/muyun/starter/MainApp.java
@@ -5,6 +5,7 @@ import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import jakarta.inject.Inject;
 import net.ximatai.muyun.core.config.MuYunConfig;
+import net.ximatai.muyun.database.core.IDatabaseOperations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,9 +17,13 @@ public class MainApp implements QuarkusApplication {
     @Inject
     MuYunConfig config;
 
+    @Inject
+    IDatabaseOperations db;
+
     @Override
     public int run(String... args) {
         logger.info("PROFILE ON {}", config.profile());
+        db.row("select 1");
         Quarkus.waitForExit();
         return 0;
     }
